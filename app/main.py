a2a0c551e94c42701aea6808474d3a9c118eeeea
whitehatbis <166512488+whitehatbis@users.noreply.github.com>
2025-11-23 04:21:21 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from app.routers.organizations import router as org_router
 from app.routers.employees import router as emp_router
 from app.routers.assignments import router as assign_router
+from app.routers.trainings import router as training_router
+
 
 app = FastAPI()
 
@@ -12,3 +14,4 @@ def root():
 app.include_router(org_router)
 app.include_router(emp_router)
 app.include_router(assign_router)
+app.include_router(training_router)
