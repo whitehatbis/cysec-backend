7f07967480c3309b8e66dc2a17d37c3662189b2a
whitehatbis <166512488+whitehatbis@users.noreply.github.com>
2025-11-22 21:57:53 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.routers.organizations import router as org_router
+from app.routers.employees import router as emp_router
 
 app = FastAPI()
 
@@ -8,3 +9,4 @@ def root():
     return {"message": "CySec Backend is running!"}
 
 app.include_router(org_router)
+app.include_router(emp_router)
