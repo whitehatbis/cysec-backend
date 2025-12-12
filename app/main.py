cdf70e9307f26bb940abb576d9dc8e0818fdb561
whitehatbis <166512488+whitehatbis@users.noreply.github.com>
2025-12-12 19:04:05 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from app.routers.trainings import router as training_router
 from app.routers.progress import router as progress_router
 from app.routers.training_progress import router as progress_update_router
 from app.routers.google_sync import router as google_sync_router
-
+from app.routers.phishing import router as phishing_router
 
 
 
@@ -23,3 +23,4 @@ app.include_router(training_router)
 app.include_router(progress_router)
 app.include_router(progress_update_router)
 app.include_router(google_sync_router)
+app.include_router(phishing_router)
