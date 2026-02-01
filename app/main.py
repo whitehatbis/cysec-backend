b8923356a9f57ae53c685ee2cdc92d03e3522fb1
whitehatbis <166512488+whitehatbis@users.noreply.github.com>
2026-02-01 09:32:12 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,6 +9,8 @@ from app.routers.progress import router as progress_router
 from app.routers.training_progress import router as progress_update_router
 from app.routers.google_sync import router as google_sync_router
 from app.routers.phishing import router as phishing_router
+from app.routers.auth import router as auth_router
+
 
 app = FastAPI()
 
@@ -36,3 +38,4 @@ app.include_router(progress_router)
 app.include_router(progress_update_router)
 app.include_router(google_sync_router)
 app.include_router(phishing_router)
+app.include_router(auth_router)
